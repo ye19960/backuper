beaf54db61e7484d550682cab67ef10213d14ed6
ye19960 <ric193@outlook.com>
2023-09-02 22:11:33 +0800

    20230903backup

--- src/backuper.cpp
+++ src/backuper.cpp
@@ -11,15 +11,73 @@ Backuper::Backuper(QWidget* parent)
 
 void Backuper::initUI()
 {
-    QLabel qlSrcPath = QLabel(this);
-    this->qlabelSrcPath.setText("源目录");
-    this->qlabelSrcPath.move(10, 10);
-    this->qlabelSrcPath.show();
+    // source path
+    this->qlSrcPath = new QLabel("源目录:", this);
+    this->qlSrcPath->move(10, 10);
 
-    this->qlabelDstPath.setText("目的目录");
-    this->qlabelDstPath.move(10, 30);
+    this->qleSrcPath = new QLineEdit(this);
+    this->qleSrcPath->move(100, 10);
+
+    this->qpbSrcPath = new QPushButton("浏览", this);
+    this->qpbSrcPath->move(300, 10);
+    // connect(this->qpbSrcPath, SIGNAL(QPushButton::clicked(this->qpbSrcPath)), this, SLOT(this->setFilePath(this->qleSrcPath)));
+    connect(this->qpbSrcPath, &QPushButton::clicked, this, [=](){this->setFilePath(this->qleSrcPath);});
+
+    // destinate path
+    this->qlDstPath = new QLabel("目的目录:", this);
+    this->qlDstPath->move(10, 50);
+
+    this->qleDstPath = new QLineEdit(this);
+    this->qleDstPath->move(100, 50);
+
+    this->qpbDstPath = new QPushButton("浏览", this);
+    this->qpbDstPath->move(300, 50);
+    connect(this->qpbDstPath, &QPushButton::clicked, this, [=](){this->setFilePath(this->qleDstPath);});
+
+
+    // other funtion button
+    this->qpbScan = new QPushButton("扫描", this);
+    this->qpbScan->move(400, 10);
+    connect(this->qpbScan, &QPushButton::clicked, this, &Backuper::scanFile);
+
+    // debug message
+    this->msgBox = new QTextEdit(this);
+    this->msgBox->resize(400, 600);
+    this->msgBox->move(600, 10);
+}
+
+void Backuper::setFilePath(QLineEdit* qle)
+{
+    // this->msgBox->append("scan button pushed.");
+    QString filePath = QFileDialog::getExistingDirectory();
+    qle->setText(filePath);
+}
+
+void Backuper::scanFile()
+{
+    QString rootPath = this->qleSrcPath->text();
+    boost::filesystem::path root(rootPath.toStdString());
+    boost::filesystem::recursive_directory_iterator rdi(root);
+    for (auto& p: rdi)
+    {
+        if (boost::filesystem::is_directory(p.path()))
+        {
+            continue;
+        }
+        this->msgBox->append(p.path().filename().string().c_str());
+    }
 }
 
 Backuper::~Backuper()
 {
+    delete this->qlSrcPath;
+    delete this->qlDstPath;
+
+    delete this->qleSrcPath;
+    delete this->qleDstPath;
+
+    delete this->qpbSrcPath;
+    delete this->qpbDstPath;
+
+    delete this->qpbScan;
 }
--- src/backuper.h
+++ src/backuper.h
@@ -2,17 +2,38 @@
 #include <QMainWindow>
 #include<QLabel>
 #include <QLineEdit>
+#include<QPushButton>
+#include<QFileDialog>
+// #include<string>
+#include<QString>
+#include<QTextEdit>
+#include<boost/filesystem.hpp>
 
 class Backuper : public QMainWindow {
     Q_OBJECT
-private:
-    // QLabel qlSrcPath;
-    // QLabel qlDstPath;
 
-    QLineEdit qleSrcPath;
-    QLineEdit qleDstPath;
+// private slots:
+//     void setFilePath(QLineEdit* qle);
+
+private:
+    QLabel* qlSrcPath;
+    QLabel* qlDstPath;
+
+    QLineEdit* qleSrcPath;
+    QLineEdit* qleDstPath;
+
+    QPushButton* qpbSrcPath;
+    QPushButton* qpbDstPath;
+
+    QPushButton* qpbScan;
+
+    QTextEdit* msgBox;
 
     void initUI();
+    void setFilePath(QLineEdit* qle);
+    void scanFile();
+
+
 
 public:
     Backuper(QWidget* parent = nullptr);
